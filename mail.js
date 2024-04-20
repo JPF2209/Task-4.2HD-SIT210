597f756b3763880d0620b2a09e816a6957d5faec
JPF2209 <105408682+JPF2209@users.noreply.github.com>
2024-04-20 17:53:33 +1000

    Update mail.js

--- mail.js
+++ mail.js
@@ -2,13 +2,13 @@
 //Get this file:///C:/Users/joshp/OneDrive/Documents/University/SIT210/Task%204.2HD/Test/index.html
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDZSR9wmDdSSOqcuXZrOrVmdmvYJ4dnN88",
-  authDomain: "test2-f11da.firebaseapp.com",
-  databaseURL: "https://test2-f11da-default-rtdb.firebaseio.com",
+  apiKey: "ApiKey",
+  authDomain: "authDomain",
+  databaseURL: "dbURL",
   projectId: "test2-f11da",
   storageBucket: "test2-f11da.appspot.com",
-  messagingSenderId: "844419963285",
-  appId: "1:844419963285:web:0be3fc22cbebe35a603e9c"
+  messagingSenderId: "messageid",
+  appId: "appid"
 };
 
 
